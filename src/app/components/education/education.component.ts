73ecc9a67c32e719ce3a9b917eb4993508d135f1
Aman agarwal <aman.agarwal@philips.com>
2020-07-11 16:17:41 +0530

    CSS svg

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -9,44 +9,26 @@ export class EducationComponent implements OnInit {
 
   constructor() { }
   public education = [{
-    catergory: 'High School',
-    CGPA: 8.4,
-    institution: 'Delhi Public School, Indore (C.B.S.E)',
-    duration: '2010-2012',
-    image: './../../../assets/teaching_f1cm.svg',
-    overlay: '10th'
-  }, {
-    catergory: 'Senior Secondary School',
-    percentage: 0.844,
-    institution: 'Delhi Public School, Indore (C.B.S.E)',
-    duration: '2012-2014',
-    additional: ['Engineering Science'],
-    image: './../../../assets/professor_8lrt.svg',
-    overlay: '12th'
-  }, {
     catergory: 'Bachelor of Technology',
     CGPA: 7.6,
     institution: 'Vellore Institute of Technology, Vellore',
     additional: ['Branch: Computer Science'],
     duration: '2015-2019',
     image: './../../../assets/Graduation_ktn0.svg',
-    overlay: 'graduation'
+  }, {
+    catergory: 'Senior Secondary School',
+    institution: 'Delhi Public School, Indore (C.B.S.E)',
+    duration: '2012-2014',
+    additional: ['Engineering Science'],
+    image: './../../../assets/professor_8lrt.svg',
+  }, {
+    catergory: 'High School',
+    institution: 'Delhi Public School, Indore (C.B.S.E)',
+    duration: '2010-2012',
+    image: './../../../assets/teaching_f1cm.svg',
+    overlay: '10th'
   }];
-  public faTimes = faTimes;
-  public faBars = faBars;
-  public expanded = false;
-  public expandedState: boolean[] = [];
-  public alter(elementNumber) {
-    let mem = this.expandedState[elementNumber];
-    for (let i = 0; i < this.expandedState.length; i++) {
-      this.expandedState[i] = false;
-    }
-    this.expandedState[elementNumber] = !mem;
-  }
   ngOnInit() {
-    for (let i = 0; i < this.education.length; i++) {
-      this.expandedState.push(false);
-    }
   }
 
 }

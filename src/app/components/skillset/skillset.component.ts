1efe89ca83c163685ad1c8f4a5082046ddd0cdf7
Aman agarwal <aman.agarwal@philips.com>
2020-11-17 06:25:59 +0530

    new skill

--- src/app/components/skillset/skillset.component.ts
+++ src/app/components/skillset/skillset.component.ts
@@ -46,9 +46,9 @@ export class SkillsetComponent implements OnInit {
     {
       skill: 'Angular',
       path: 'angular.svg'
-    },{
-      skill:'NodeJS',
-      path:'nodejs.svg'
+    }, {
+      skill: 'NodeJS',
+      path: 'nodejs.svg'
     },
     {
       skill: 'Webpack',
@@ -81,89 +81,15 @@ export class SkillsetComponent implements OnInit {
     {
       skill: 'Postman',
       path: 'postman.svg'
+    },
+    {
+      skill: 'Docker',
+      path: 'docker.svg'
+    }, {
+      skill: 'Kubernates',
+      path: 'kubernetes.svg'
     }
   ];
-  public skills =
-    [{
-      Domain: 'Competitive Coding',
-      set: [
-        {
-          skill: 'C++',
-          level: 0.60
-        }, {
-          skill: 'Python',
-          level: 0.85
-        },
-        {
-          skill: 'Java',
-          level: 0.75
-        }
-      ]
-    },
-    {
-      Domain: 'Front-End Web Development',
-      set: [
-        {
-          skill: 'HTML',
-          level: 0.80
-        },
-        {
-          skill: 'CSS',
-          level: 0.85
-        }, {
-          skill: 'JS',
-          level: 0.80
-        }
-      ]
-    },
-    {
-      Domain: 'Web Framework',
-      set: [
-        {
-          skill: 'Vanilla',
-          level: 0.90
-        },
-        {
-          skill: 'Angular',
-          level: 0.85
-        }
-      ]
-    }, {
-      Domain: 'Backend Frameworks',
-      set: [
-        {
-          skill: 'Flask',
-          level: 0.75
-        },
-        {
-          skill: 'Spring',
-          level: 0.75
-        }
-      ]
-    },
-    {
-      Domain: 'Databases',
-      set: [
-        {
-          skill: 'Postgres',
-          level: 0.80
-        },
-        {
-          skill: 'MongoDB',
-          level: 0.70
-        }
-      ]
-    },
-    {
-      Domain: 'Version Control',
-      set: [
-        {
-          skill: 'Git',
-          level: 0.90
-        }
-      ]
-    },
-    ];
   constructor() { }
 
   ngOnInit() {
